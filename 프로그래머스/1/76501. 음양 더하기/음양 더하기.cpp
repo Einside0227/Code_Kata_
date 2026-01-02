d56e082d8b95387f7f7502662b718d58a398a33c
PARKDULNAE <moung051@naver.com>
2026-01-02 09:26:14 +0900

    [level 1] Title: 음양 더하기, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int sum = 0;
+    for(int i = 0; i<absolutes.size(); i++){
+        if(!signs[i]){
+            absolutes[i] *= -1;
+        }
+        sum += absolutes[i];
+    }
+    return sum;
+}
