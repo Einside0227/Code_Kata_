32e398f7838af5ddc17eb91627bb8cca7fb09db2
PARKDULNAE <moung051@naver.com>
2026-01-14 09:47:17 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> ternary;
+
+    while (n > 0) {
+        ternary.push_back(n % 3);
+        n /= 3;
+    }
+
+    reverse(ternary.begin(), ternary.end());
+
+    int power = 1;
+    for (int i = 0; i < ternary.size(); i++) {
+        answer += ternary[i] * power;
+        power *= 3; 
+    }
+
+    return answer;
+}
