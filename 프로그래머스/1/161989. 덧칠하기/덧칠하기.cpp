872507b62b1193f0da9de0dc7eb2a7df428a98c8
PARKDULNAE <moung051@naver.com>
2026-02-10 09:35:50 +0900

    [level 1] Title: 덧칠하기, Time: 0.33 ms, Memory: 5.51 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int end = 0;
+    
+    for (int s : section) {
+        if (s > end) {
+            answer++;
+            end = s + m - 1;
+        }
+    }
+        
+    return answer;
+}
